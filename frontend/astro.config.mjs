6b0c2c88efff455c74ccc3f98a1768d63b46984d
alexmontanad <alexmontanad@gmail.com>
2025-12-11 09:06:05 -0500

    fix: Allow Railway host in Vite config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,21 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
-    })
+    }),
   ],
-  output: 'static'
+  server: {
+    host: true,
+  },
+  vite: {
+    server: {
+      allowedHosts: true, // Permitir todos los hosts
+    },
+  },
+  output: "static",
 });
